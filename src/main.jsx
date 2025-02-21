d6c9eb45ef4b6f6f3ebfb1950077bb329cd4fc83
rizwanahmed1981 <razykan.ra@gmail.com>
2025-02-21 14:27:35 +0500

    complete sidebar context

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 // product provider
-import ProductProvider from './contexts/ProductContext.jsx'
+import ProductProvider from "./contexts/ProductContext.jsx";
+// sidebar Provider
+import SidebarProvider from "./contexts/SidebarContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <ProductProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </ProductProvider>
-)
+createRoot(document.getElementById("root")).render(
+  <SidebarProvider>
+    <ProductProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </ProductProvider>
+  </SidebarProvider>
+);
