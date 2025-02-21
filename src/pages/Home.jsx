de430a62ee5f296df1e75c041c5e1962a157c2f3
rizwanahmed1981 <razykan.ra@gmail.com>
2025-02-21 13:33:44 +0500

    completely fetched products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,31 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 // import product context
-import { ProductContext } from '../contexts/ProductContext'
+import { ProductContext } from "../contexts/ProductContext";
+// import product component
+import Product from "../components/Product";
 
 const Home = () => {
   // get products from product context
   const { products } = useContext(ProductContext);
   // get onlt men's & women's clothing category
-  const filteredProducts = products.filter(item => {
-    return item.category === "men's clothing" || item.category === "women's clothing";
+  const filteredProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
   });
   return (
     <div>
-      <section className='py-16'>
-        <div className="mx-auto px-[30px] lg:px-0">
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 xl:gris-cols-5 gap-[30px]'>
+      <section className="py-16">
+        <div className="mx-auto px-[30px]">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:gris-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
             {filteredProducts.map((product) => {
-              return (
-                <div key={product.id} className='w-full h-[300px] bg-pink-200'>{product.title}</div>
-              )
+              return <Product product={product} key={product.id} />;
             })}
           </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
